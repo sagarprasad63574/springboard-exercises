17a47c0952bdd50477f0a016c323f5fc56e0ce52
Sagar Prasad <sagarprasad63574@gmail.com>
2023-02-13 19:22:52 -0800

    worked on the flask cupcake project

---
+++ flask-cupcakes/app.py
@@ -0,0 +1,66 @@
+"""Flask app for Cupcakes"""
+from flask import Flask, request, jsonify, render_template
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+connect_db(app)
+
+def check_image(image):
+    if image == "" or image == None: 
+        return None
+    else: 
+        return image
+
+@app.route('/')
+def index_page():
+    """Renders html template that includes some JS - NOT PART OF JSON API!"""
+    return render_template('index.html')
+
+@app.route('/api/cupcakes')
+def list_all_cupcakes():
+    """Returns JSON with all cupcakes"""
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=all_cupcakes)
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    """Returns JSON for one cupcake in particular"""
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcake():
+    """Creates a new cupcake and returns JSON of that created cupcake"""
+    image = check_image(request.json["image"])
+    new_cupcake = Cupcake(flavor=request.json["flavor"], size=request.json["size"], 
+                        rating=request.json["rating"], image = image)
+    db.session.add(new_cupcake)
+    db.session.commit()
+    response_json = jsonify(cupcake=new_cupcake.serialize())
+    return (response_json, 201)
+
+@app.route('/api/cupcakes/<int:id>', methods=["PATCH"])
+def update_cupcake(id):
+    """Updates a particular cupcake and responds with JSON of that updated cupcake"""
+    cupcake = Cupcake.query.get_or_404(id)
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
+    cupcake.size = request.json.get('size', cupcake.size)
+    cupcake.rating = request.json.get('rating', cupcake.rating)
+    cupcake.image = request.json.get('image', cupcake.image)
+    db.session.commit()
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route('/api/cupcakes/<int:id>', methods=["DELETE"])
+def delete_cupcake(id):
+    """Deletes a particular cupcake"""
+    cupcake = Cupcake.query.get_or_404(id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify(message="deleted")
---
+++ flask-cupcakes/static/cupcakes.js
@@ -0,0 +1,46 @@
+const BASE_URL = "http://localhost:5000/api";
+
+function generateCupcakeHTML(cupcake) {
+  return `
+    <div data-cupcake-id=${cupcake.id}>
+      <li>
+        ${cupcake.flavor} / ${cupcake.size} / ${cupcake.rating}
+      </li>
+      <img class="Cupcake-img"
+            src="${cupcake.image}"
+            alt="(no image provided)">
+    </div>
+  `;
+}
+
+async function showInitialCupcakes() {
+  const response = await axios.get(`${BASE_URL}/cupcakes`);
+
+  for (let cupcakeData of response.data.cupcakes) {
+    let newCupcake = $(generateCupcakeHTML(cupcakeData));
+    $("#cupcakes-list").append(newCupcake);
+  }
+}
+
+$("#cupcake-form").on("submit", async function (evt) {
+  evt.preventDefault();
+
+  let flavor = $("#flavor").val();
+  let rating = $("#rating").val();
+  let size = $("#size").val();
+  let image = $("#image").val();
+
+  const newCupcakeResponse = await axios.post(`${BASE_URL}/cupcakes`, {
+    flavor,
+    rating,
+    size,
+    image
+  });
+
+  let newCupcake = $(generateCupcakeHTML(newCupcakeResponse.data.cupcake));
+  $("#cupcakes-list").append(newCupcake);
+  $("#cupcake-form").trigger("reset");
+});
+
+
+$(showInitialCupcakes);
