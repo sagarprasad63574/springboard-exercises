8a56a0a078146773307010bc4aa12038ae44c34f
Sagar Prasad <saprasad@csumb.edu>
2023-04-05 11:38:03 -0700

    react cards pokemon

--- react/react-cards-pokemon/src/PlayingCardList.js
+++ react/react-cards-pokemon/src/PlayingCardList.js
@@ -2,6 +2,7 @@ import React from "react";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
 import {useAxios} from "./hooks.js";
+import { formatCard } from "./helpers";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
@@ -14,12 +15,12 @@ function CardTable() {
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
+        <button onClick={() => addCard(formatCard)}>Add a playing card!</button>
         <button onClick={clearCards}>Clear the table</button>
       </div>
       <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+        {cards.map(card => (
+          <PlayingCard key={card.id} front={card.image} />
         ))}
       </div>
     </div>
